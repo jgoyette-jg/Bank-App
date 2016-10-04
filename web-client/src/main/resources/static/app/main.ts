f7943547a79586e466649ebb6208bead209e9169
Jeff Goyette <jgoyette.jg@gmail.com>
2016-10-03 20:36:11 -0500

    Created first module and component

---
+++ web-client/src/main/resources/static/app/main.ts
@@ -0,0 +1,5 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import {BankModule} from './modules/bank.module';
+
+platformBrowserDynamic().bootstrapModule(BankModule);
