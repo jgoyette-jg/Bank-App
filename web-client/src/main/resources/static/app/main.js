405b359286d76d8de56903a41511463225ae9720
jgoyette-jg <jgoyette.jg@gmail.com>
2016-10-31 19:03:43 -0500

    Cleaned up source by removing generated files

--- web-client/src/main/resources/static/app/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var bank_module_1 = require('./modules/bank.module');
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(bank_module_1.BankModule);
-//# sourceMappingURL=main.js.map
